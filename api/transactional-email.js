656c9c2f3b284c0251e273a247206c5587d8d708
Blake Petersen <blakepetersen@gmail.com>
2021-03-21 16:27:25 -0700

    trans email endpoint

---
+++ api/transactional-email.js
@@ -0,0 +1,33 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+module.exports = (req, res) => {
+
+    if (req.body !== 'ding') {
+        return;
+    }
+
+
+    const msg = {
+        to: 'blakepetersen@gmail.com', // Change to your recipient
+        from: 'bpetersen@twilio.com', // Change to your verified sender
+        subject: 'Sending with SendGrid is Fun',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+
+    sgMail
+        .send(msg)
+        .then(() => {
+            res.json({
+                body: 'success!',
+                status: 200
+            })
+        })
+        .catch((error) => {
+            res.json({
+                body: `error: ${error}`,
+                status: 200
+            })
+        })
+}
