b607bd6986e9a195b40178dc939616ac73b8db3a
Blake Petersen <blakepetersen@gmail.com>
2021-03-22 12:16:27 -0700

    enable cors for api

--- next.config.js
+++ next.config.js
@@ -11,6 +11,20 @@ module.exports = withManifest(withOffline({
 		CONTENTFUL_ACCESS_TOKEN: nconf.get('CONTENTFUL_ACCESS_TOKEN'),
 		CONTENTFUL_MANAGEMENT_TOKEN: nconf.get('CONTENTFUL_MANAGEMENT_TOKEN'),
 	},
+	async headers() {
+		return [
+			{
+				// matching all API routes
+				source: "/api/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+					{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+				]
+			}
+		]
+	},
 	webpack: function (config) {
 		config.module.rules.push({
 			test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
