79e7f0144611b1fd736185f2295ae20571e131c2
Blake Petersen <blake@blakepetersen.io>
2022-01-22 00:32:10 -0800

    fix: clean up nav

--- apps/blakepetersen.io/src/components/nav/main.tsx
+++ apps/blakepetersen.io/src/components/nav/main.tsx
@@ -98,49 +98,9 @@ const _Subtitle = styled.div`
   line-height: 1.5rem;
 `
 
-// const _PageTitle = styled.h2`
-//   max-width: 1000px;
-//   width: calc(50vw + 300px);
-//   min-width: 300px;
-//   opacity: 0;
-//   font-size: 3rem;
-//   font-weight: bold;
-//   transform: scale(0.8) translateY(-400px);
-//   transition: opacity 150ms ease, transform 300ms ease, font-size 300ms ease-out;
-//
-//   .is-post & {
-//     margin: -5rem 0 -4rem;
-//     transform: scale(0.6) translateY(0);
-//     opacity: 1;
-//   }
-//
-//   .full.is-post & {
-//     margin: 0;
-//     transform: scale(1) translateY(0);
-//   }
-// `
-
-// const _Meta = styled.div`
-//   .is-post & {
-//     padding: 1.25rem;
-//     transition: opacity 150ms ease-out, transform 300ms ease-out;
-//     opacity: 0;
-//     transform: translateY(100px);
-//   }
-//
-//   .full.is-post & {
-//     opacity: 1;
-//     transform: translateY(0);
-//   }
-// `
-
 const MainNav = ({ isTop = true }) => {
   return (
     <_MainNav className={`main-nav full`}>
-      {/*<svg viewBox="0 0 8 8" id="menu" className="icon" onClick={this.toggleNavMenu}>*/}
-      {/*<path d="M0 1v1h8v-1h-8zm0 2.969v1h8v-1h-8zm0 3v1h8v-1h-8z" id="menu"></path>*/}
-      {/*</svg>*/}
-
       <_Content>
         <_Logo>
           <Link href={`/`}>
@@ -150,24 +110,7 @@ const MainNav = ({ isTop = true }) => {
             </a>
           </Link>
         </_Logo>
-
-        {/*{ this.props.pageTitle &&*/}
-        {/*<_PageTitle>*/}
-        {/*<Link prefetch href="/"><a>{ this.props.pageTitle }</a></Link>*/}
-        {/*</_PageTitle>*/}
-        {/*}*/}
-
-        {/*{ this.props.pageDate &&*/}
-        {/*<_Meta>*/}
-        {/*<Moment format={`MMMM Do, YYYY`}>{ this.props.pageDate }</Moment>*/}
-        {/*</_Meta>*/}
-        {/*}*/}
       </_Content>
-
-      {/*<svg viewBox="0 0 8 8" id="search" className="icon" onClick={this.toggleSearchMenu}>*/}
-      {/*<path d="M3.5 0c-1.927 0-3.5 1.573-3.5 3.5s1.573 3.5 3.5 3.5c.592 0 1.166-.145 1.656-.406a1 1 0 0 0 .125.125l1 1a1.016 1.016 0 1 0 1.438-1.438l-1-1a1 1 0 0 0-.156-.125c.266-.493.438-1.059.438-1.656 0-1.927-1.573-3.5-3.5-3.5zm0 1c1.387 0 2.5 1.113 2.5 2.5 0 .661-.241 1.273-.656 1.719l-.031.031a1 1 0 0 0-.125.125c-.442.397-1.043.625-1.688.625-1.387 0-2.5-1.113-2.5-2.5s1.113-2.5 2.5-2.5z" id="magnifying-glass"></path>*/}
-      {/*</svg>*/}
-
       <Triangles />
     </_MainNav>
   )
