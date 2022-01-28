f99b96fb7e932c04e2ab07ab3d9a517de7d53d3b
Blake Petersen <blake@blakepetersen.io>
2022-01-28 15:33:15 -0800

    fix: use stitches instead of styled

--- apps/ashleypetersenphoto.com/src/components/social-links.tsx
+++ apps/ashleypetersenphoto.com/src/components/social-links.tsx
@@ -1,46 +1,44 @@
-import Link from "next/link";
-import styled from "styled-components";
+import { styled } from '@stitches/react'
 
-const _Link = styled.a`
-    margin: 0.5rem;
-`;
+import Link from 'next/link'
+
+const _Link = styled('a', {
+  margin: '0.5rem',
+})
 
 export default function SocialLinks() {
-    return (
-        <nav>
-            <Link href="https://www.facebook.com/ashleypetersenphoto" passHref>
-                <_Link>Facebook</_Link>
-            </Link>
+  return (
+    <nav>
+      <Link href="https://www.facebook.com/ashleypetersenphoto" passHref>
+        <_Link>Facebook</_Link>
+      </Link>
 
-            <Link
-                href="https://www.linkedin.com/in/ashleypetersenphoto"
-                passHref
-            >
-                <_Link>LinkedIn</_Link>
-            </Link>
+      <Link href="https://www.linkedin.com/in/ashleypetersenphoto" passHref>
+        <_Link>LinkedIn</_Link>
+      </Link>
 
-            <Link href="https://www.pinterest.com/ashley_petersen/" passHref>
-                <_Link>Pinterest</_Link>
-            </Link>
+      <Link href="https://www.pinterest.com/ashley_petersen/" passHref>
+        <_Link>Pinterest</_Link>
+      </Link>
 
-            <Link href="https://twitter.com/ashleydpetersen" passHref>
-                <_Link>Twitter</_Link>
-            </Link>
+      <Link href="https://twitter.com/ashleydpetersen" passHref>
+        <_Link>Twitter</_Link>
+      </Link>
 
-            <Link href="https://500px.com/ashleypetersen" passHref>
-                <_Link>500px</_Link>
-            </Link>
+      <Link href="https://500px.com/ashleypetersen" passHref>
+        <_Link>500px</_Link>
+      </Link>
 
-            <Link href="https://instagram.com/ashleypetersen" passHref>
-                <_Link>Instagram</_Link>
-            </Link>
+      <Link href="https://instagram.com/ashleypetersen" passHref>
+        <_Link>Instagram</_Link>
+      </Link>
 
-            <Link
-                href="https://www.youtube.com/channel/UCrdXvnJxT6A7OdL4bilbKzQ"
-                passHref
-            >
-                <_Link>Youtube</_Link>
-            </Link>
-        </nav>
-    );
+      <Link
+        href="https://www.youtube.com/channel/UCrdXvnJxT6A7OdL4bilbKzQ"
+        passHref
+      >
+        <_Link>Youtube</_Link>
+      </Link>
+    </nav>
+  )
 }
