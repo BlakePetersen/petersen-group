d60a7fc09df84a2cad91203c84029916c230b693
Blake Petersen <blake@blakepetersen.io>
2022-01-28 16:08:39 -0800

    feat: get button state stubbed out and connecting to connectors

--- apps/ashleypetersenphoto.com/src/components/connect-wallet.tsx
+++ apps/ashleypetersenphoto.com/src/components/connect-wallet.tsx
@@ -1,5 +1,26 @@
-function WalletConnector() {
-  return <a>Connect Wallet</a>
+import { useAccount, useConnect } from 'wagmi'
+import { Content, Item, Root, Trigger } from '@radix-ui/react-dropdown-menu'
+
+const WalletConnector = () => {
+  const [{ data: connectData, loading: connectDataLoading, error }, connect] =
+    useConnect()
+  const [{ data: accountData }] = useAccount()
+
+  console.log(connectData)
+
+  return (
+    <Root>
+      <Trigger>Connect Wallet</Trigger>
+
+      <Content>
+        {connectData.connectors.map((connector, i) => (
+          <Item key={i} onClick={() => connect(connector)}>
+            {connector.name}
+          </Item>
+        ))}
+      </Content>
+    </Root>
+  )
 }
 
 export default WalletConnector
