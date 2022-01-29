18d961636ab8586aed4eab32e95998d29f850d10
Blake Petersen <blake@blakepetersen.io>
2022-01-28 22:55:45 -0800

    feat: remove head component

--- apps/ashleypetersenphoto.com/src/pages/index.tsx
+++ apps/ashleypetersenphoto.com/src/pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next'
-
-import Head from '@/components/head'
+import Head from 'next/head'
 
 const Index: NextPage = () => {
   return (
     <>
-      <Head
-        title={`Conceptual Portraiture, Underwater Photography - Ashley Petersen Photography`}
-      />
+      <Head>
+        <title>
+          Conceptual Portraiture, Underwater Photography - Ashley Petersen
+          Photography
+        </title>
+      </Head>
+
+      <body>Body</body>
     </>
   )
 }
