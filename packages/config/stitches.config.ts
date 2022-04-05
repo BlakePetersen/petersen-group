0d3540024c9f6d49d123ca2ff1eb3962e01b0d87
Blake Petersen <blake@blakepetersen.io>
2022-04-04 21:04:35 -0700

    feat: add breakpoints to stitches base config

--- packages/config/stitches.config.ts
+++ packages/config/stitches.config.ts
@@ -18,6 +18,11 @@ export const {
   },
   media: {
     bp1: '(min-width: 480px)',
+    bp2: '(min-width: 640px)',
+    bp3: '(min-width: 768px)',
+    bp4: '(min-width: 1024px)',
+    bp5: '(min-width: 1280px)',
+    bp6: '(min-width: 1600px)',
   },
   utils: {
     marginX: value => ({ marginLeft: value, marginRight: value }),
