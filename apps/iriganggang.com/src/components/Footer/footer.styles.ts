85e6a8fb131c7067f3ce2fc5928d91dac84196ff
Blake Petersen <blake@blakepetersen.io>
2022-05-03 00:58:14 -0700

    feat: twitter attribution

--- apps/iriganggang.com/src/components/Footer/footer.styles.ts
+++ apps/iriganggang.com/src/components/Footer/footer.styles.ts
@@ -1,5 +1,5 @@
 import { styled } from '@stitches/react'
-import { blackA, violet } from '@radix-ui/colors'
+import { blackA, violet, whiteA } from '@radix-ui/colors'
 import * as AvatarPrimitive from '@radix-ui/react-avatar'
 
 export const _Footer = styled('footer', {
@@ -7,7 +7,6 @@ export const _Footer = styled('footer', {
   display: 'flex',
   alignItems: `center`,
   justifyContent: `center`,
-  gap: `.25rem`,
   lineHeight: `1`,
   bottom: '0',
   width: '100%',
@@ -26,12 +25,12 @@ export const _Avatar = styled(AvatarPrimitive.Root, {
   width: `3rem`,
   height: `3rem`,
   borderRadius: '100%',
-  backgroundColor: blackA.blackA3,
-  border: `2px solid white`,
-  boxShadow: `0 3px 1px rgba(0, 0, 0, .1), 0 6px 2px rgba(0, 0, 0, .1)`,
+  backgroundColor: whiteA.whiteA12,
+  border: `3px solid ${whiteA.whiteA12}`,
+  // boxShadow: `0 3px 1px rgba(0, 0, 0, .1), 0 6px 2px rgba(0, 0, 0, .1)`,
   transition: 'box-shadow 200ms ease, transform 200ms ease',
   '&:hover': {
-    boxShadow: `0 6px 2px rgba(0, 0, 0, .075), 0 12px 4px rgba(0, 0, 0, .075)`,
+    // boxShadow: `0 6px 2px rgba(0, 0, 0, .075), 0 12px 4px rgba(0, 0, 0, .075)`,
     transform: `translateY(-3px)`,
   },
 })
@@ -49,27 +48,20 @@ export const _AvatarFallback = styled(AvatarPrimitive.Fallback, {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  backgroundColor: 'white',
+  backgroundColor: whiteA.whiteA12,
   color: violet.violet11,
   fontSize: 15,
   lineHeight: 1,
   fontWeight: 500,
 })
 
-export const _Attribution = styled('div', {
-  textShadow: `0 3px 1px rgba(0, 0, 0, .1), 0 6px 2px rgba(0, 0, 0, .1)`,
+export const _Attribution = styled('div', {})
+
+export const _Handle = styled('div', {
+  transform: `translateY(-.75rem)`,
+  color: blackA.blackA12,
+  backgroundColor: whiteA.whiteA12,
+  borderRadius: `999999px`,
 })
 
-export const _AttributionLink = styled('a', {
-  display: `inline-block`,
-  textShadow: `0 3px 1px rgba(0, 0, 0, .1), 0 6px 2px rgba(0, 0, 0, .1)`,
-  transition: 'text-shadow 200ms ease, transform 200ms ease',
-  '&:hover': {
-    textShadow: `0 6px 2px rgba(0, 0, 0, .075), 0 12px 4px rgba(0, 0, 0, .075)`,
-    transform: `translateY(-3px)`,
-  },
-  '&:focus, &:active': {
-    textShadow: `0 2px 1px rgba(0, 0, 0, .125), 0 4px 2px rgba(0, 0, 0, .125)`,
-    transform: `translateY(1px)`,
-  },
-})
+export const _Copyright = styled('div', {})
