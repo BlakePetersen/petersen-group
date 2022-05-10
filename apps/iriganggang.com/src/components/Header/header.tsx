fac86ab24c65f819576b4901580b0991b77a4b49
Blake Petersen <blake@blakepetersen.io>
2022-05-10 14:15:55 -0700

    feat: add header component to igg

---
+++ apps/iriganggang.com/src/components/Header/header.tsx
@@ -0,0 +1,8 @@
+import { _Header } from './header.styles'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+
+export const Header = () => (
+  <_Header>
+    <ConnectButton />
+  </_Header>
+)
