6456d05021485022573b2a1a0cbb8195d63407d1
Blake Petersen <blake@blakepetersen.io>
2022-06-23 23:15:47 -0700

    fix: remove menu wrapper

--- apps/iriganggang.com/src/components/Header/header.tsx
+++ apps/iriganggang.com/src/components/Header/header.tsx
@@ -1,14 +1,12 @@
 import { _Header } from './header.styles'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { Menu } from 'artax-ui'
 import Wordmark from '@/components/Wordmark'
 import { HeaderTypes } from './header.types'
 
 export const Header: HeaderTypes = () => (
   <_Header>
-    {/*<Menu>*/}
     <ConnectButton label={'Sign In'} showBalance={false} chainStatus="none" />
-    {/*</Menu>*/}
+
     <Wordmark />
   </_Header>
 )
