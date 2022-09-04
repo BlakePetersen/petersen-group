f886ab4315ad6ca10c7d7c7abf84905df5676938
Blake Petersen <blake@blakepetersen.io>
2022-09-03 21:34:46 -0700

    chore: add axiom to 0xbp

--- apps/iriganggang.com/next.config.js
+++ apps/iriganggang.com/next.config.js
@@ -3,15 +3,16 @@
 const withPlugins = require('next-compose-plugins')
 const path = require('path')
 const withTM = require('next-transpile-modules')(['artax-ui'])
+const {withAxiom} = require('next-axiom');
 
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['degentoonz-app-gfcxi.ondigitalocean.app', 'gateway.ipfs.io'],
-  },
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    images: {
+        domains: ['degentoonz-app-gfcxi.ondigitalocean.app', 'gateway.ipfs.io'],
+    },
 }
 
-module.exports = withPlugins([withTM], nextConfig)
+module.exports = withPlugins([withAxiom, withTM], nextConfig)
