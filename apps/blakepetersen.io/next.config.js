5b9d116507b9c117f0ad898c7ea06ee50b0efd2e
Blake Petersen <blake@blakepetersen.io>
2022-09-03 20:38:37 -0700

    chore: add axiom to bpio

--- apps/blakepetersen.io/next.config.js
+++ apps/blakepetersen.io/next.config.js
@@ -3,32 +3,33 @@
  */
 const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['artax-ui'])
+const {withAxiom} = require('next-axiom');
 
 const nextConfig = {
-  compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
-      },
-    ]
-  },
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+    },
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    {key: 'Access-Control-Allow-Credentials', value: 'true'},
+                    {key: 'Access-Control-Allow-Origin', value: '*'},
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value:
+                            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
-module.exports = withPlugins([withTM], nextConfig)
+module.exports = withPlugins([withAxiom, withTM], nextConfig)
--- apps/blakepetersen.io/src/pages/_app.tsx
+++ apps/blakepetersen.io/src/pages/_app.tsx
@@ -1,22 +1,24 @@
-import type { AppProps } from 'next/app'
-import { AnimatePresence } from 'framer-motion'
+import type {AppProps} from 'next/app'
+import {AnimatePresence} from 'framer-motion'
+
+export {reportWebVitals} from 'next-axiom';
 
 import Layout from '@/components/layout'
 
 import '@/styles/_global-styles.scss'
 
-const MyApp = ({ Component, pageProps, router }: AppProps) => {
-  return (
-    <Layout>
-      <AnimatePresence
-        exitBeforeEnter
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
-    </Layout>
-  )
+const MyApp = ({Component, pageProps, router}: AppProps) => {
+    return (
+        <Layout>
+            <AnimatePresence
+                exitBeforeEnter
+                initial={false}
+                onExitComplete={() => window.scrollTo(0, 0)}
+            >
+                <Component {...pageProps} key={router.route}/>
+            </AnimatePresence>
+        </Layout>
+    )
 }
 
 export default MyApp
