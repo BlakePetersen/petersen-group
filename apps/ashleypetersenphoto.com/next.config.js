953d92c8256a3c22ffdca05aeb424218fb63f9c6
Blake Petersen <blake@blakepetersen.io>
2022-09-03 20:24:21 -0700

    chore: ad axiom to app

--- apps/ashleypetersenphoto.com/next.config.js
+++ apps/ashleypetersenphoto.com/next.config.js
@@ -2,15 +2,16 @@
 
 const path = require('path')
 const withPlugins = require('next-compose-plugins')
+const {withAxiom} = require('next-axiom');
 const withTM = require('next-transpile-modules')(['artax-ui'])
 
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['mint.sacredskullsnft.io', 'cdn.sanity.io'],
-  },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    images: {
+        domains: ['mint.sacredskullsnft.io', 'cdn.sanity.io'],
+    },
 }
 
-module.exports = withPlugins([withTM], nextConfig)
+module.exports = withPlugins([withTM, withAxiom], nextConfig)
