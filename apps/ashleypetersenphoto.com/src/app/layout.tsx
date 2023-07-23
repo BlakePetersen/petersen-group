1b898b0af9134b55cf0a888aa11fdc1339ea23bb
Blake Petersen <blake@blakepetersen.io>
2023-07-23 14:36:21 -0700

    chore: font subset add

--- apps/ashleypetersenphoto.com/src/app/layout.tsx
+++ apps/ashleypetersenphoto.com/src/app/layout.tsx
@@ -1,11 +1,11 @@
-import { Lora } from "next/font/google"
+import { Lora } from 'next/font/google'
 
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 
 import '@/styles/global-styles.scss'
 
-const lora = Lora()
+const lora = Lora({ subsets: ['latin'] })
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
